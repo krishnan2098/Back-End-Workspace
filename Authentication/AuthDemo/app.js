dbab9b24dc79a0b440d5612bf1f8fadbebd4d357
krishnan2098 <krishnan2098@gmail.com>
2018-03-25 20:36:26 +0000

    added signup functionality with authentication using passport

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -8,7 +8,23 @@ var express = require("express"),
     passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret:"Rusty is the cutest dog in the world",
+    resave: "false",
+    saveUninitialized:"false"
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//===============
+// ROUTES
+//===============
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -18,6 +34,33 @@ app.get("/secret", (req,res) => {
    res.render("secret") ;
 });
 
+//Auth routes
+//Show sign up form
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register", (req, res) => {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.redirect("register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/secret");
+        });
+    });
+});
+
+//LOGIN ROUTES
+//Render Login forms
+app.get("/login", (req, res) => {
+    res.render)("login");
+});
+
 app.listen(process.env.PORT,process.env.IP, () => {
     console.log("server has started ...");
-})
+});
