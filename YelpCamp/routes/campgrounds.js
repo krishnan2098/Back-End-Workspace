96b2bdd46f578c8ec3b50997ff0f12dc91d6cd07
krishnan2098 <krishnan2098@gmail.com>
2018-03-30 05:33:01 +0000

    Added edit and update routes

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -14,24 +14,30 @@ router.get("/", (req, res) => {
 });
 
 //CREATE -  Add new campground to DB
-router.post("/", ( req, res) => {
+router.post("/", isLoggedIn, ( req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = { name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+
+    var newCampground = { name: name, image: image, description: desc, author: author};
     //Create a new campground and save to DB
     Campground.create(newCampground, (err, newlyCreated) => {
         if(err){
             console.log(err);
         } else {
-            // redirect to campgrounds page    
+            // redirect to campgrounds page   
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
 });
 
 //NEW - show form to create new campground
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("campgrounds/new");
 });
 
@@ -48,5 +54,37 @@ router.get("/:id", (req, res) => {
         }
     });
 });
+
+//EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", (req, res) => {
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });
+});
+
+//UPDATE CAMPGROUND ROUTE
+router.put("/:id", (req,res) => {
+    //find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+        //redirect to show page
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+//middleware checking if user is still logged in
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
