07461eeab341738bcba9100187d7edc5ec7b7a89
krishnan2098 <krishnan2098@gmail.com>
2018-04-02 06:25:17 +0000

    Added edit and delete routes, also added authorisation middleware

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -48,6 +48,62 @@ router.post("/", (req, res) => {
     });
 });
 
+//COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+       if(err) {
+            res.redirect("/campgrounds/" + req.params.id);
+       } else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment});
+       }
+    });
+});
+
+//COMMENT UPDATE ROUTE
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedCampground) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+})
+
+//middleware to handle user authorisation
+function checkCommentOwnership(req, res, next){
+    //is user logged in?
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if(err){
+                console.log(err);
+            } else {
+                //does user own the campground?
+                if(foundComment.author.id.equals(req.user.id)){
+                    next();
+                //otherwise redirect
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    //if not redirect
+    } else {
+        res.redirect("back");
+    }
+}
+
 //middleware checking if user is still logged in
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
